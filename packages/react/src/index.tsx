656462888b860137c644a7d66f2a10b1d661263c
pedro henrique <pedro.henrike2812@gmail.com>
2024-12-16 10:44:24 -0300

    feat: configurando stitches

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,10 @@
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+})
+
+export function App() {
+  return <Button>Hello World!</Button>
+}
